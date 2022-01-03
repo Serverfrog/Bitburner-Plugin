a43e9eab051b4cf2369c190a8ed8e01b154ea6d7
GitHub Action <action@github.com>
2022-01-03 04:20:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/serverfrog/bitburnerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.serverfrog.bitburnerplugin.services
+
+import com.github.serverfrog.bitburnerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
