e719737aae8b21d5af5786b7231ee4050de3f2f1
Bastian Venz <bastian-venz@t-online.de>
2022-07-29 10:21:49 +0200

    #15 Updated Version and some libs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,10 +4,21 @@
 
 ## [Unreleased]
 
+## [0.0.4]
+
+Updated Plugin org.jetbrains.kotlin.jvm to 1.7.10
+Updated Plugin org.jetbrains.intellij to 1.7.0
+Added Plugin com.github.ben-manes.versions
+Updated `pluginUntilBuild` to 222.*
+Updated used Gradle Version to 7.5
+
 ## [0.0.3]
+
 added PR #12
 
 ## [0.0.2]
+
 ### Added
+
 - Filewatcher which automatically pushes to Bitburner
 - Updated IDEA Version
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,15 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.4.0"
+    id("org.jetbrains.intellij") version "1.7.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
+    // Dependency Updates
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 group = properties("pluginGroup")
